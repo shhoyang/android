b20be3740c931d2247a47f8660b16983ecee465b
杨世豪 <haoyshi@163.com>
2021-03-08 11:22:00 +0800

    Initial commit

--- library/src/main/java/com/hao/library/ui/WebActivity.kt
+++ library/src/main/java/com/hao/library/ui/WebActivity.kt
@@ -3,25 +3,36 @@ package com.hao.library.ui
 import android.content.Context
 import android.content.Intent
 import android.view.WindowManager
-import com.hao.library.annotation.AndroidEntryPoint
-import com.hao.library.databinding.ActivityWebBinding
+import android.widget.ProgressBar
+import androidx.lifecycle.ViewModel
+import androidx.viewbinding.ViewBinding
+import com.hao.library.R
+import com.hao.library.annotation.Base
 import com.hao.library.view.web.ProgressAnimHelper
+import com.hao.library.view.web.ProgressWebView
 import com.hao.library.view.web.WebViewLoadListener
-import com.hao.library.viewmodel.PlaceholderViewModel
 
-@AndroidEntryPoint(injectViewModel = false)
-class WebActivity : BaseActivity<ActivityWebBinding, PlaceholderViewModel>(),
+@Base
+class WebActivity<VB : ViewBinding, VM : ViewModel> : BaseActivity<VB, VM>(),
     WebViewLoadListener {
+
+    private var webView: ProgressWebView? = null
+    private var progressBar: ProgressBar? = null
 
     private var progressAnimHelper: ProgressAnimHelper? = null
 
     override fun initView() {
         window.addFlags(WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED)
-        viewBinding {
-            baseWebView.setWebViewLoadListener(this@WebActivity)
-            progressAnimHelper = ProgressAnimHelper(baseProgressBar)
-            title = intent.getStringExtra(TITLE)
-            baseWebView.doLoadUrl(intent.getStringExtra(URL) ?: "")
+        webView = f(R.id.baseWebView)
+        progressBar = f(R.id.baseProgressBar)
+        title = intent.getStringExtra(TITLE)
+
+        if (progressBar != null) {
+            progressAnimHelper = ProgressAnimHelper(progressBar!!)
+        }
+        webView?.apply {
+            setWebViewLoadListener(this@WebActivity)
+            doLoadUrl(intent.getStringExtra(URL) ?: "")
         }
     }
 
@@ -31,13 +42,13 @@ class WebActivity : BaseActivity<ActivityWebBinding, PlaceholderViewModel>(),
 
     override fun onDestroy() {
         progressAnimHelper?.destroy()
-        viewBinding { baseWebView.destroy() }
+        webView?.destroy()
         super.onDestroy()
     }
 
     override fun onBackPressed() {
-        if (vb?.baseWebView != null && vb!!.baseWebView.canGoBack()) {
-            vb!!.baseWebView.goBack()
+        if (webView != null && webView!!.canGoBack()) {
+            webView!!.goBack()
         } else {
             super.onBackPressed()
         }
@@ -52,6 +63,10 @@ class WebActivity : BaseActivity<ActivityWebBinding, PlaceholderViewModel>(),
 
     override fun progressChanged(newProgress: Int) {
         progressAnimHelper?.progressChanged(newProgress)
+    }
+
+    fun getWevView(): ProgressWebView? {
+        return webView
     }
 
     companion object {
